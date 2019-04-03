032b3ce4264d45597bfd38a74b2d4f7e7242aeed
tpeterson6 <46914262+tpeterson6@users.noreply.github.com>
2019-04-03 11:52:05 -0700

    Initial commit

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour {
+    public float speed;
+    private Rigidbody rb;
+    private int count;
+    public Text countText;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        countText.text = "Tryptacoins: " + count.ToString();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText ();
+            
+        }
+    }
+    void SetCountText ()
+    {
+        countText.text = "Tryptacoins: " + count.ToString();
+    }
+
+}
